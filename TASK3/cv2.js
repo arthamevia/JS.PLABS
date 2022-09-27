eecfc80962dfe4cf74f1dfd1d3c86e225421f712
arthamevia <yuli45904@gmail.com>
2022-09-27 11:02:50 +0700

    Task 3 Personal Profile

--- TASK3/cv2.js
+++ TASK3/cv2.js
@@ -1,11 +1,16 @@
 
-            //    Durasi magang
-            let start = document.getElementById('start').innerHTML = 'September 07, 2022';
-            let end = document.getElementById('end').innerHTML = 'December 22, 2022';
+            // sisa Durasi magang
+            let start = document.getElementById('start').innerHTML = 'september 07 2022';
+            let end = document.getElementById('end').innerHTML = 'Sekarang';
 
-            start = new Date(start).getMonth();
-            end = new Date(end).getMonth();
+            let bulan_start = new Date(start).getMonth();
+            let bulan_end = new Date().getMonth();
 
-            let duration = end - start;
+            let bulan = bulan_end - bulan_start;
 
-            document.getElementById('duration').innerHTML = `Durasi : ${duration} Bulan`;
+            let day_start = new Date(start).getDate();
+            let day_end = new Date().getDate();
+
+            let day = day_end - day_start;
+
+            document.getElementById('duration').innerHTML = `Durasi : ${bulan} bulan ${day} day `;
