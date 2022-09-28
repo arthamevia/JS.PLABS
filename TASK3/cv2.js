c5d10e873d38ac77145877ec7e970c2a2a07074c
arthamevia <yuli45904@gmail.com>
2022-09-28 11:14:45 +0700

    Task 3 Personal Profile

--- TASK3/cv2.js
+++ TASK3/cv2.js
@@ -3,14 +3,14 @@
             let start = document.getElementById('start').innerHTML = 'september 07 2022';
             let end = document.getElementById('end').innerHTML = 'Sekarang';
 
-            let bulan_start = new Date(start).getMonth();
-            let bulan_end = new Date().getMonth();
+            let month_start = new Date(start).getMonth();
+            let month_end = new Date().getMonth();
 
-            let bulan = bulan_end - bulan_start;
+            let month = month_end - month_start;
 
             let day_start = new Date(start).getDate();
             let day_end = new Date().getDate();
 
             let day = day_end - day_start;
 
-            document.getElementById('duration').innerHTML = `Durasi : ${bulan} bulan ${day} day `;
+            document.getElementById('duration').innerHTML = `Durasi : ${month} month ${day} day `;
